97392aeed81515bfa8ec6a17a45c9138207ebcf8
Anandmohanty <mohantyanand3@gmail.com>
2023-02-20 20:46:53 +0530

    [Anand] - Cabin Invoice Generator

---
+++ CabinVoiceGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CabinVoiceGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
